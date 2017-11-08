facc9c917aa43bbe42ef8190a07343d3078c9394
Shen Chauhan <shenchauhan@users.noreply.github.com>
2017-11-07 21:08:56 -0800

    Updating the Winforms solution to the new calculation libraries

--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -6,20 +6,15 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
-        private double result;
-        private string operation;
-
-        private readonly CalculationHistory calculationHistory = new CalculationHistory();
-
         public Form1()
         {
             InitializeComponent();
-            calculationHistory.ClearHistory();
+            CalculationHistory.ClearHistory();
         }
 
         private void btn_Click(object sender, EventArgs e)
         {
-            if (lblResult.Text  == "0" | lblResult.Text == "/" | lblResult.Text == "+" | lblResult.Text == "-" | lblResult.Text == "x")
+            if (lblResult.Text == "0" | lblResult.Text == "/" | lblResult.Text == "+" | lblResult.Text == "-" | lblResult.Text == "x")
             {
                 lblResult.Text = string.Empty;
             }
@@ -27,81 +22,19 @@ namespace Calculator
             lblResult.Text += (sender as Button)?.Text;
         }
 
-        private void btnDivide_Click(object sender, EventArgs e)
-        {
-            Calculate();
-            operation = "/";
-            calculationHistory.AddToCalculation(lblResult.Text + operation);
-            lblResult.Text = operation;
-        }
-
-        private void btnMultiply_Click(object sender, EventArgs e)
-        {
-            Calculate();
-            operation = "x";
-            calculationHistory.AddToCalculation(lblResult.Text + operation);
-            lblResult.Text = operation;
-        }
-
-        private void btnSubtract_Click(object sender, EventArgs e)
-        {
-            Calculate();
-            operation = "-";
-            calculationHistory.AddToCalculation(lblResult.Text + operation);
-            lblResult.Text = operation;
-        }
-
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-            Calculate();
-            operation = "+";
-            calculationHistory.AddToCalculation(lblResult.Text + operation);
-            lblResult.Text = operation;
-        }
-
         private void btnEquals_Click(object sender, EventArgs e)
         {
-            Calculate();
-            calculationHistory.AddToCalculation(lblResult.Text);
-            lblResult.Text = result.ToString();
-            calculationHistory.AddToHistory(result);
+            lblResult.Text = Calculation.NetStandard.Calculator.Calculate(lblResult.Text).ToString();
 
             lbHistory.SelectedIndexChanged -= new EventHandler(lbHistory_SelectedIndexChanged);
-            lbHistory.DataSource = calculationHistory.FetchEntireHistory();
+            lbHistory.DataSource = CalculationHistory.FetchEntireHistory();
             lbHistory.SelectedIndexChanged += new EventHandler(lbHistory_SelectedIndexChanged);
-
-            result = 0;
-            operation = string.Empty;
-        }
-
-        private void Calculate()
-        {
-            var x = double.Parse(lblResult.Text);
-
-            switch (operation)
-            {
-                case "/":
-                    result /= x;
-                    break;
-                case "+":
-                    result += x;
-                    break;
-                case "-":
-                    result -= x;
-                    break;
-                case "x":
-                    result *= x;
-                    break;
-                default:
-                    result = x;
-                    break;
-            }
         }
 
         private void lbHistory_SelectedIndexChanged(object sender, EventArgs e)
         {
             var selectedItem = lbHistory.SelectedItem.ToString();
-            lblResult.Text = calculationHistory.FetchFromHistory(selectedItem).ToString();
+            lblResult.Text = CalculationHistory.FetchFromHistory(selectedItem).ToString();
         }
 
         private void btnC_Click(object sender, EventArgs e)
@@ -111,10 +44,7 @@ namespace Calculator
 
         private void btnCE_Click(object sender, EventArgs e)
         {
-            result = 0;
-            lblResult.Text = "0";
-            operation = string.Empty;
-            calculationHistory.Clear();
+            lblResult.Text = string.Empty;
         }
     }
 }
